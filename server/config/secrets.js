b55ddd61f13265d2eceea6c3c30409aac0e40ee3
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-10 13:40:43 -0600

    add files (cherry picked from commit 711cd73)

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,17 +1,21 @@
-import config from '../../etc/no-share.json';
+if (!process.env.GOOGLE_CLIENTID) {
+var config={};
+    config = require('../../etc/no-share.json');
+
+}
 
 export default function () {
     // Find the appropriate database to connect to, default to localhost if not found.
     return {
         sessionSecret: 'random-session-string',
         google: {
-            clientID: config.googleClientID,
-            clientSecret: config.googleClientSecret,
+            clientID: process.env.GOOGLE_CLIENTID || config.googleClientID,
+            clientSecret: process.env.GOOGLE_CLIENTSECRET || config.googleClientSecret,
             callbackURL: "/auth/google/callback"
         },
         facebook: {
-            clientID: config.facebookClientID,
-            clientSecret: config.facebookClientSecret,
+            clientID: process.env.FACEBOOK_CLIENTID || config.facebookClientID,
+            clientSecret: process.env.FACEBOOK_CLIENTSECRET || config.facebookClientSecret,
             callbackURL: "/auth/facebook/callback"
         }
     }
