fd14d79743917c0f758d9f6315cc4e6df8f344f7
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-10 13:08:48 -0600

    add files (cherry picked from commit 8d6abc3)

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,21 +1,18 @@
-if (!process.env.GOOGLE_CLIENTID) {
-var config={};
-    config = require('../../etc/no-share.json');
-
-}
+import config from '../../etc/no-share.json';
 
 export default function () {
     // Find the appropriate database to connect to, default to localhost if not found.
     return {
+        db: 'mongodb://redux:FeCtut@ds039175.mongolab.com:39175/redux',
         sessionSecret: 'random-session-string',
         google: {
-            clientID: process.env.GOOGLE_CLIENTID || config.googleClientID,
-            clientSecret: process.env.GOOGLE_CLIENTSECRET || config.googleClientSecret,
+            clientID: config.googleClientID,
+            clientSecret: config.googleClientSecret,
             callbackURL: "/auth/google/callback"
         },
         facebook: {
-            clientID: process.env.FACEBOOK_CLIENTID || config.facebookClientID,
-            clientSecret: process.env.FACEBOOK_CLIENTSECRET || config.facebookClientSecret,
+            clientID: config.facebookClientID,
+            clientSecret: config.facebookClientSecret,
             callbackURL: "/auth/facebook/callback"
         }
     }
