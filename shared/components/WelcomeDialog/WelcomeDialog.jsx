359b5fedd26a82789a707b57c7a2c52e29dcd676
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-10 16:01:16 -0600

    Cached

--- shared/components/WelcomeDialog/WelcomeDialog.jsx
+++ shared/components/WelcomeDialog/WelcomeDialog.jsx
@@ -9,7 +9,7 @@ import Checkbox    from '../../../node_modules/react-mdl/lib/Checkbox';
 import Isvg from 'react-inlinesvg';
 
 if (process.env.BROWSER) {
-    require('./WelcomeDialog.less');
+    require('!style!css!less!./WelcomeDialog.less');
 }
 
 export default class WelcomeDialog extends Component {
