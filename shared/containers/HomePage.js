73662a5beed3ea79c8c8284065a72267a8816ee9
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-01-11 12:05:36 -0600

    Home page route works

--- shared/containers/HomePage.js
+++ shared/containers/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AppBar      from './../components/AppBar/AppBar.jsx';
+
 export default class HomePage extends React.Component {
     // Constructor
     constructor(props) {
@@ -10,6 +12,12 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div>
+                <AppBar
+                    title={'Chicago Web App'}
+                    className='ArticlesPage__app-bar'
+                    fixOnScroll={false}
+                    scrollOffset={65}
+                />
                 Home Page Content
             </div>
         )
