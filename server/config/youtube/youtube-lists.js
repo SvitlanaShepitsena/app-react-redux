3027fe8fbccd11fecd40a5006c94408414a20e1b
Svitlana Shepitsena <chicagobusinessintelligence1@gmail.com>
2016-01-12 15:25:01 -0600

    Routes. About

--- server/config/youtube/youtube-lists.js
+++ server/config/youtube/youtube-lists.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import parser from 'fast-html-parser';
+import cheerio from 'cheerio';
+import cheerio from 'cheerio';
+
+
 
 export default  (req, res)=> {
-    axios.get('http://www.screencast.com/users/SvitlanaShepitsena/folders/Reactjs').then((result)=> {
+    axios.get('https://www.youtube.com/watch?v=DKUimZvRyqg&list=PLPgDBCA1Cb3NXTI0Ceqpw3GqXBrv7rjld').then((result)=> {
         let html = result.data;
+        let $ = cheerio.load(html);
+        const videos = $('#playlist-autoscroll-list');
+        console.log(videos);
+
+
+        res.end('success');
 
     });
 
