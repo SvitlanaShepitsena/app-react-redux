c25d14febbe0372a7a34586b5ed19ddf27608765
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-12 14:58:50 -0600

    L

--- server/config/youtube/youtube-lists.js
+++ server/config/youtube/youtube-lists.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 import parser from 'fast-html-parser';
 
 export default  (req, res)=> {
-    axios.get('https://www.youtube.com/watch?v=xjX_-VjsUfU&list=PLPgDBCA1Cb3Ngjpo21aFkdqVT2H0fZu3W').then((res)=> {
-        console.log(res.data);
-        res.status(200).end('s');
-    });
+    axios.get('http://www.screencast.com/users/SvitlanaShepitsena/folders/Reactjs').then((result)=> {
+        let html = result.data;
 
+    });
 
 }
