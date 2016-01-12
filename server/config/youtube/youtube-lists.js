2372fc4e3aa7d592af6b5f4c05a3924e5e1b1f5d
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-12 16:26:49 -0600

    Youtube list

--- server/config/youtube/youtube-lists.js
+++ server/config/youtube/youtube-lists.js
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import parser from 'fast-html-parser';
-import cheerio from 'cheerio';
-import cheerio from 'cheerio';
-
-
+import  cheerio from 'cheerio';
+import select from 'cheerio-select';
 
 export default  (req, res)=> {
     axios.get('https://www.youtube.com/watch?v=DKUimZvRyqg&list=PLPgDBCA1Cb3NXTI0Ceqpw3GqXBrv7rjld').then((result)=> {
         let html = result.data;
         let $ = cheerio.load(html);
-        const videos = $('#playlist-autoscroll-list');
-        console.log(videos);
+        var list = [];
+        $('#playlist-autoscroll-list').find('li a').each(function (i, elem) {
+            let el = $(elem).attr('href').split('=')[1].split('&')[0];
+            list.push(el);
+        });
 
 
-        res.end('success');
+        res.json(list);
 
+    }).catch((error)=> {
+        console.log(error);
     });
 
 }
