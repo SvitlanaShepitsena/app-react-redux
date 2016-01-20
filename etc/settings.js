ce60b06a89a55b553b7c0af40ae70ce456c709f9
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 16:15:49 -0600

    add files

--- etc/settings.js
+++ etc/settings.js
@@ -7,16 +7,18 @@ export default function (host) {
     let jsSettings = JSON.stringify(clientConfig);
     if (process.env.NODE_ENV !== 'development') {
         jsSettings = jsSettings.replace(/(3001)|(8050)/g, PORT);
-
-        if (host.indexOf('local')===-1) {
+        if (host.indexOf('local') === -1) {
             jsSettings = jsSettings.replace(/localhost/g, host);
             jsSettings = jsSettings.replace(/http/g, 'https');
             // if on Heroku
 
-            jsSettings.staticUrl = jsSettings.staticUrl.substr(0, -5);
         }
 
     }
-    return JSON.parse(jsSettings);
+    jsSettings = JSON.parse(jsSettings);
+    if (host.indexOf('local') === -1) {
+        jsSettings.staticUrl = jsSettings.staticUrl.substr(0, jsSettings.staticUrl.length - 5);
+    }
+    return jsSettings;
 };
 
