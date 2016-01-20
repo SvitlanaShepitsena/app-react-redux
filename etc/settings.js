c633bdf40d98612432daa35f2fb5868079fe4eac
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 13:59:11 -0600

    Server

--- etc/settings.js
+++ etc/settings.js
@@ -5,6 +5,6 @@ import clientConfig from './client-config.json';
 import PORT from '../server/port';
 export default function () {
 
-    return JSON.parse(JSON.stringify(clientConfig).replace(/3001/g, PORT));
+    return JSON.parse(JSON.stringify(clientConfig).replace(/(3001)|(8050)/g, PORT));
 }();
 
