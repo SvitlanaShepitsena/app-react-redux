ea819345de6ca26d85aaf2061c77d6009a2c5d28
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 15:03:27 -0600

    Heroku preparation

--- etc/settings.js
+++ etc/settings.js
@@ -3,12 +3,16 @@
 import clientConfig from './client-config.json';
 
 import PORT from '../server/port';
-export default function () {
+export default function (host) {
     let jsSettings = JSON.stringify(clientConfig);
     if (process.env.NODE_ENV === 'production') {
         jsSettings = jsSettings.replace(/(3001)|(8050)/g, PORT);
 
+        //if (host.indexOf('local')===-1) {
+            jsSettings = jsSettings.replace(/localhost/g, host);
+        //}
+
     }
     return JSON.parse(jsSettings);
-}();
+};
 
