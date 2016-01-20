56b93f00028ef0d130aa834707bdd9eeca71f290
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 11:12:53 -0600

    add files

--- shared/containers/pages/HomePage.js
+++ shared/containers/pages/HomePage.js
@@ -12,7 +12,7 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <Helmet title="Chicago Web App"/>
+                <Helmet title="Chicago Web App232"/>
                 <HomeContent></HomeContent>
             </div>
         )
