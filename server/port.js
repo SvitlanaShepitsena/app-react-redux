c6ef62253363adb99a281ab87955e62ae8addbaa
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 15:09:57 -0600

    add files

--- server/port.js
+++ server/port.js
@@ -1,7 +1,6 @@
 export default function () {
     console.log(process.env.NODE_ENV);
     var port = process.env.NODE_ENV == 'production' ? 80 : 3001;
-    console.log(port);
 
-    return port;
+    return process.env.PORT || port;
 }()
