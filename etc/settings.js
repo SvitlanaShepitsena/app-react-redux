3def5d26efb1ffbe632b1d70df73297015bd1e6e
AndriyShepitsen <chicagobusinessintelligence2@gmail.com>
2016-01-20 15:58:44 -0600

    add files

--- etc/settings.js
+++ etc/settings.js
@@ -5,7 +5,7 @@ import clientConfig from './client-config.json';
 import PORT from '../server/port';
 export default function (host) {
     let jsSettings = JSON.stringify(clientConfig);
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV !== 'development') {
         jsSettings = jsSettings.replace(/(3001)|(8050)/g, PORT);
 
         if (host.indexOf('local')===-1) {
